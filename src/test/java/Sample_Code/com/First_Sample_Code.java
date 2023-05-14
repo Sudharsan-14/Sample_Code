34c9f62cf9536534699d93615ea22ad364927b10
Sudharsan-14 <sudharsanveluchamy@gmail.com>
2023-05-14 20:24:13 +0530

    first_commit

---
+++ src/test/java/Sample_Code/com/First_Sample_Code.java
@@ -0,0 +1,46 @@
+package Sample_Code.com;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import epicList_Chrome_Account_User.Utility;
+
+public class First_Sample_Code {
+	public WebDriver driver;
+	
+	@Test
+	public void Test_run() throws Throwable {
+		
+		Thread.sleep(2000);
+		// Call the chrome driver
+		System.setProperty(
+	            "webdriver.chrome.driver",
+	            "C:\\Automation\\WebPOS\\chromedriver.exe");
+		// Open the Chrome window
+		driver = new ChromeDriver();
+		
+		// Wait for 30 seconds
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		// Maximize the Chrome window
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		//Launch the URL
+		driver.get("https://id.atlassian.com/login?continue=https%3A%2F%2Fstart.atlassian.com%2F&application=start");
+		
+		Thread.sleep(5000);
+		
+		driver.close();
+		
+		driver.quit();
+		
+		
+
+	}
+	
+	
+	
+
+}
